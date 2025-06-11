d9e207bb20631da4fe42e582ba62dcf09da08864
maheswari <mahi.sgs22@gmail.com>
2025-06-11 16:43:09 +0530

    terraform

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -1,37 +1,35 @@
-# data "aws_ami" "mahi" {
-#   owners      = ["557690617909"]
-#   most_recent = true
-#   filter {
-#     name   = "name"
-#     values = ["RHEL-9-DevOps-Practice"]
-#   }
-# }
-
-# output "ami-id" {
-#   value = data.aws_ami.mahi.id
-# }
-
-#data "aws_ami" "mahi" 
-data "aws_ami" "joindevops" {
-  owners      = ["557690617909"]
+data "aws_ami" "mahi" {
+  owners      = ["973714476881"]
   most_recent = true
-
   filter {
     name   = "name"
-    values = ["RHEL-9-DevOps-Practice"] 
-  }
-   filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    values = ["RHEL-9-DevOps-Practice"]
   }
 }
 
 output "ami-id" {
-  value = data.aws_ami.joindevops.id
+  value = data.aws_ami.mahi.id
 }
+
+# data "aws_ami" "joindevops" {
+#   owners      = ["973714476881"]
+#   most_recent = true # latest version lo crate chestudi ee command
+#   filter {
+#     name   = "name"
+#     values = ["RHEL-9-DevOps-Practice"]
+#   }
+#   filter {
+#     name   = "root-device-type"
+#     values = ["ebs"]
+#   }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+# }
+# output "ami_id" {
+#   value = data.aws_ami.joindevops.id
+# }
+
+
 
