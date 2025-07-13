c4d52544de3bcf80b968a58fd24146e4495d7815
maheswari <mahi.sgs22@gmail.com>
2025-07-13 16:00:45 +0530

    terraform

--- secure-state/ec2.tf
+++ secure-state/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [ aws_security_group.allow_all.id ]
   tags = {
-    Name = "HelloWorld"
+    Name = "HelloWorldddd"
   }
 }
 
