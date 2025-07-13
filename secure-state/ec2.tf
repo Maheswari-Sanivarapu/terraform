f82d95030566a77d59c99589d806e0dee0324327
maheswari <mahi.sgs22@gmail.com>
2025-07-13 16:04:04 +0530

    terraform

--- secure-state/ec2.tf
+++ secure-state/ec2.tf
@@ -8,30 +8,30 @@ resource "aws_instance" "ec2_instance" {
 }
 
 
-# resource "aws_security_group" "allow_all" {
-#     name = "allow-alll"
-#     description = "allow all traffic"
+resource "aws_security_group" "allow_all" {
+    name = "allow-alll"
+    description = "allow all traffic"
 
-#     ingress {
-#         from_port = 0
-#         to_port = 0
-#         protocol = "-1"
-#         cidr_blocks = ["0.0.0.0/0"]
-#         ipv6_cidr_blocks = ["::/0"]
-#     }
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
 
-#     egress {
-#         from_port = 0
-#         to_port = 0
-#         protocol = "-1"
-#         cidr_blocks = ["0.0.0.0/0"]
-#         ipv6_cidr_blocks = ["::/0"]
-#     }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
 
-#     tags = {
-#         Name = "Allow-all"
-#     }
-#     lifecycle {
-#       create_before_destroy = true
-#     }
-# }
+    tags = {
+        Name = "Allow-all"
+    }
+    lifecycle {
+      create_before_destroy = true
+    }
+}
